6ab89aa93bcea33ee5b36f2700518ba93bd46c94
lamba <lam45883@gmail.com>
2025-04-10 10:02:12 +0700

    fix update schedule

--- src/pages/student/schedule/all-schedule/ViewAllSchedule.jsx
+++ src/pages/student/schedule/all-schedule/ViewAllSchedule.jsx
@@ -31,7 +31,7 @@ function ViewAllSchedule() {
   };
 
   return (
-    <div className="flex flex-col space-y-6 p-6">
+    <div className="flex flex-col p-6 h-full">
       <div className="bg-white rounded-lg shadow-md p-6">
         <header className="flex justify-between items-center mb-6">
           <h2 className="text-2xl font-semibold text-gray-800 border-b-2 border-blue-500 pb-2">
@@ -58,8 +58,8 @@ function ViewAllSchedule() {
             No sessions found
           </div>
         ) : (
-          <div className="overflow-x-auto">
-            <table className="min-w-full divide-y divide-gray-200">
+          <div className="h-[calc(100vh-300px)] overflow-y-auto">
+            <table className="min-w-full h-full divide-y divide-gray-200">
               <thead className="bg-gray-50">
                 <tr>
                   <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
@@ -95,7 +95,7 @@ function ViewAllSchedule() {
                       {new Date(schedule.startTime).toLocaleString()}
                     </td>
                     <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-                      {schedule.duration} mins
+                      {schedule.sessionDuration} mins
                     </td>
                     <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
                       {schedule.meetingType === "online" ? (
